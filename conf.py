7215fc08f6b78160c0071ac4e8bd9f5cf2452daa
xuyuping <xuyuping1993@qq.com>
2019-08-15 14:30:02 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -34,28 +34,19 @@ release = u''
 #
 # needs_sphinx = '1.0'
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 source_suffix = ['.rst', '.md']
 # source_suffix = '.rst'
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     'sphinx_markdown_tables', 
-    'sphinx.ext.autosectionlabel'
+    'sphinx.ext.autodoc'
 ]
-
-autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
