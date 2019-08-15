48d889b5bbdf7ab6ee5bd479b6d2776e768dbd12
xuyuping <xuyuping1993@qq.com>
2019-08-15 12:56:25 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -38,8 +38,8 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
-    'sphinx_markdown_tables'
+    'recommonmark', 
+    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
